3c7e5722a72bf5d4c5a6770f2717ece4a41eac79
Koos85 <koosjoosten@gmail.com>
2025-11-17 22:52:20 +0100

    probe lib v2

--- lib/check/phpfpm.py
+++ lib/check/phpfpm.py
@@ -1,53 +1,55 @@
 import aiohttp
 import logging
 from libprobe.asset import Asset
+from libprobe.check import Check
 from libprobe.exceptions import CheckException
 from libprobe.exceptions import IgnoreCheckException
 from ..connector import get_connector
 
 
-async def check_phpfpm(
-        asset: Asset,
-        asset_config: dict,
-        check_config: dict):
+class CheckPhpFpm(Check):
+    key = 'phpfpm'
 
-    try:
-        url = check_config['statusUrl']
-    except Exception:
-        logging.warning(
-            'Check did not run; '
-            'statusUrl is not provided, invalid or empty')
-        raise IgnoreCheckException
+    @staticmethod
+    async def run(asset: Asset, local_config: dict, config: dict) -> dict:
 
-    try:
-        async with aiohttp.ClientSession(connector=get_connector()) as session:
-            async with session.get(
-                url,
-                params={'json': '1'},
-                ssl=False
-            ) as resp:
-                resp.raise_for_status()
-                data = await resp.json()
-    except Exception as e:
-        msg = str(e) or type(e).__name__
-        raise CheckException(msg)
+        try:
+            url = config['statusUrl']
+        except Exception:
+            logging.warning(
+                'Check did not run; '
+                'statusUrl is not provided, invalid or empty')
+            raise IgnoreCheckException
 
-    return {
-        'phpfpm': [{
-            'name': 'phpfpm',
-            'pool': data.get('pool'),
-            'process_manager': data.get('process manager'),
-            'start_time': data.get('start time'),
-            'start_since': data.get('start since'),
-            'accepted_conn': data.get('accepted conn'),
-            'listen_queue': data.get('listen queue'),
-            'max_listen_queue': data.get('max listen queue'),
-            'listen_queue_len': data.get('listen queue len'),
-            'idle_processes': data.get('idle processes'),
-            'active_processes': data.get('active processes'),
-            'total_processes': data.get('total processes'),
-            'max_active_processes': data.get('max active processes'),
-            'max_children_reached': data.get('max children reached'),
-            'slow_requests': data.get('slow requests'),
-        }],
-    }
+        try:
+            async with aiohttp.ClientSession(connector=get_connector()) as se:
+                async with se.get(
+                    url,
+                    params={'json': '1'},
+                    ssl=False
+                ) as resp:
+                    resp.raise_for_status()
+                    data = await resp.json()
+        except Exception as e:
+            msg = str(e) or type(e).__name__
+            raise CheckException(msg)
+
+        return {
+            'phpfpm': [{
+                'name': 'phpfpm',
+                'pool': data.get('pool'),
+                'process_manager': data.get('process manager'),
+                'start_time': data.get('start time'),
+                'start_since': data.get('start since'),
+                'accepted_conn': data.get('accepted conn'),
+                'listen_queue': data.get('listen queue'),
+                'max_listen_queue': data.get('max listen queue'),
+                'listen_queue_len': data.get('listen queue len'),
+                'idle_processes': data.get('idle processes'),
+                'active_processes': data.get('active processes'),
+                'total_processes': data.get('total processes'),
+                'max_active_processes': data.get('max active processes'),
+                'max_children_reached': data.get('max children reached'),
+                'slow_requests': data.get('slow requests'),
+            }],
+        }
